2d2648312cb8d1c0237d823835c8ea4a6366a16c
hhh67 <hhio.67.2020@gmail.com>
2023-06-16 22:11:27 +0900

    initial commit

---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by hhh67 on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
