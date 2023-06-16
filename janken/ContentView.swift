0d391cd9be9f60a054478599095a82b199cd3518
hhh67 <hhio.67.2020@gmail.com>
2023-06-16 22:45:39 +0900

    チュートリアル

--- janken/ContentView.swift
+++ janken/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPressed: Bool = false;
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(isPressed ? "Hi, Swift!" : "Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.pink)
+            Button("切り替えボタン") {
+                isPressed.toggle()
+            }
+            .padding(.horizontal, 12)
+            .padding(/*@START_MENU_TOKEN@*/.vertical, 10.0/*@END_MENU_TOKEN@*/)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+            .cornerRadius(8)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
         }
-        .padding()
     }
 }
 
